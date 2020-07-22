e8b098b9044aad6bdd5c185ec9346ead3440d6bb
Marius Lundgård <studio@mariuslundgard.com>
2020-07-22 08:54:17 +0200

    Update prettier config

--- babel.config.js
+++ babel.config.js
@@ -1,39 +1,39 @@
-"use strict";
+'use strict'
 
-const isTest = process.env.NODE_ENV === "test";
+const isTest = process.env.NODE_ENV === 'test'
 
 const testConfig = {
-  presets: ["@babel/preset-env"],
+  presets: ['@babel/preset-env'],
   plugins: [
-    "@babel/plugin-proposal-object-rest-spread",
+    '@babel/plugin-proposal-object-rest-spread',
     [
-      "@babel/plugin-transform-react-jsx",
+      '@babel/plugin-transform-react-jsx',
       {
-        pragma: "element",
-        pragmaFrag: "fragment",
+        pragma: 'element',
+        pragmaFrag: 'fragment',
         throwIfNamespace: false
       }
     ]
   ]
-};
+}
 
 const buildConfig = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
         modules: false,
         targets: {
-          chrome: "62",
-          edge: "16",
-          firefox: "56",
+          chrome: '62',
+          edge: '16',
+          firefox: '56',
           // ios_saf: "11",
-          safari: "11"
+          safari: '11'
         }
       }
     ]
   ],
-  plugins: ["@babel/plugin-proposal-object-rest-spread"]
-};
+  plugins: ['@babel/plugin-proposal-object-rest-spread']
+}
 
-module.exports = isTest ? testConfig : buildConfig;
+module.exports = isTest ? testConfig : buildConfig
--- src/hypp.js
+++ src/hypp.js
@@ -1,16 +1,16 @@
 export function addEvents(elm, events) {
   Object.keys(events).forEach(eventType => {
-    elm.addEventListener(eventType, events[eventType]);
-  });
+    elm.addEventListener(eventType, events[eventType])
+  })
 }
 
 export function addStyle(elm, style) {
-  if (typeof style === "string") {
-    elm.style = style;
+  if (typeof style === 'string') {
+    elm.style = style
   } else {
     Object.keys(style).forEach(prop => {
-      elm.style[prop] = style[prop];
-    });
+      elm.style[prop] = style[prop]
+    })
   }
 }
 
@@ -18,131 +18,131 @@ const elementClasses = {
   div: HTMLDivElement,
   pre: HTMLPreElement,
   ul: HTMLUListElement
-};
-
-const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
-function createRandomString(len = 5) {
-  let str = "";
-  let i;
-
-  for (i = 0; i < len; i += 1) {
-    str += LOWER_CASE.charAt(Math.floor(Math.random() * LOWER_CASE.length));
-  }
-
-  return str;
 }
 
-function createUniqueCustomElementName(prefix = "x") {
-  let key = null;
-  while (!key) {
-    const k = `${prefix}-${createRandomString()}`;
-    key = !customElements.get(k) && k;
+const LOWER_CASE = 'abcdefghijklmnopqrstuvwxyz'
+function createRandomString(len = 5) {
+  let str = ''
+  let i
+
+  for (i = 0; i < len; i += 1) {
+    str += LOWER_CASE.charAt(Math.floor(Math.random() * LOWER_CASE.length))
   }
-  return key;
+
+  return str
+}
+
+function createUniqueCustomElementName(prefix = 'x') {
+  let key = null
+  while (!key) {
+    const k = `${prefix}-${createRandomString()}`
+    key = !customElements.get(k) && k
+  }
+  return key
 }
 
 function createLifecycleElement(name, hooks) {
-  const BaseClass = elementClasses[name];
+  const BaseClass = elementClasses[name]
 
   if (!BaseClass) {
-    throw new Error(`No base class for <${name}>`);
+    throw new Error(`No base class for <${name}>`)
   }
 
-  const is = createUniqueCustomElementName();
+  const is = createUniqueCustomElementName()
 
   class XElement extends BaseClass {
     adoptedCallback(...args) {
       if (hooks.adopt) {
-        hooks.adopt(...args);
+        hooks.adopt(...args)
       }
     }
     attributeChangedCallback(...args) {
       if (hooks.attributeChange) {
-        hooks.attributeChange(...args);
+        hooks.attributeChange(...args)
       }
     }
     connectedCallback(...args) {
       if (hooks.connect) {
-        hooks.connect(...args);
+        hooks.connect(...args)
       }
     }
     disconnectedCallback(...args) {
       if (hooks.disconnect) {
-        hooks.disconnect(...args);
+        hooks.disconnect(...args)
       }
     }
   }
 
   if (hooks.observedAttributes) {
-    XElement.observedAttributes = hooks.observedAttributes;
+    XElement.observedAttributes = hooks.observedAttributes
   }
 
-  customElements.define(is, XElement, { extends: name });
+  customElements.define(is, XElement, {extends: name})
 
-  return document.createElement(name, { is });
+  return document.createElement(name, {is})
 }
 
-const CUSTOM_ATTRS = ["events", "hooks", "style"];
+const CUSTOM_ATTRS = ['events', 'hooks', 'style']
 export function element(name, props, ...children) {
-  props = props || {};
+  props = props || {}
 
-  if (typeof name === "string") {
+  if (typeof name === 'string') {
     const elm = props.hooks
       ? createLifecycleElement(name, props.hooks)
-      : document.createElement(name);
+      : document.createElement(name)
 
     if (props.events) {
-      addEvents(elm, props.events);
+      addEvents(elm, props.events)
     }
 
     if (props.style) {
-      addStyle(elm, props.style);
+      addStyle(elm, props.style)
     }
 
     Object.keys(props).forEach(attrKey => {
       if (CUSTOM_ATTRS.indexOf(attrKey) === -1) {
-        elm.setAttribute(attrKey, props[attrKey]);
+        elm.setAttribute(attrKey, props[attrKey])
       }
-    });
+    })
 
-    elm.appendChild(fragment(children));
+    elm.appendChild(fragment(children))
 
-    return elm;
+    return elm
   } else {
-    props.children = fragment(children);
+    props.children = fragment(children)
 
-    return name(props);
+    return name(props)
   }
 }
 
 export function empty(elm) {
-  const len = elm.childNodes.length;
+  const len = elm.childNodes.length
 
-  let i;
+  let i
 
   for (i = len - 1; i >= 0; i -= 1) {
-    elm.removeChild(elm.childNodes[i]);
+    elm.removeChild(elm.childNodes[i])
   }
 }
 
 export function fragment(children) {
-  const frag = document.createDocumentFragment();
+  const frag = document.createDocumentFragment()
 
-  children = Array.isArray(children) ? children : [children.children];
+  children = Array.isArray(children) ? children : [children.children]
 
-  if (Array.isArray(children[0])) children = children[0];
+  if (Array.isArray(children[0])) children = children[0]
 
   children.filter(Boolean).forEach(child => {
-    if (typeof child === "string") {
-      frag.appendChild(text(child));
+    if (typeof child === 'string') {
+      frag.appendChild(text(child))
     } else {
-      frag.appendChild(child);
+      frag.appendChild(child)
     }
-  });
+  })
 
-  return frag;
+  return frag
 }
 
 export function text(str) {
-  return document.createTextNode(str);
+  return document.createTextNode(str)
 }
